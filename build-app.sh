5e6418e768742de1c2974fbb5aea2f5dc32041da
Hill <hill@kimo.com>
2021-01-02 09:49:34 +0800

    change to use slim

---
+++ build-app.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+DOCKER_FILE=$DIR/Dockerfile.app
+
+sourceImage="hillliu/django-pandas-opencv:app"
+
+build() {
+  sourceImage=$sourceImage DOCKER_FILE=${DOCKER_FILE} $DIR/compile.sh b
+}
+
+push() {
+  sourceImage=$sourceImage DOCKER_FILE=${DOCKER_FILE} $DIR/compile.sh p app 
+}
+
+usage() {
+echo -n "
+  Usage: $0 [b|p]
+  b    Build App
+  p    Push App 
+
+"
+}
+
+
+case "$1" in
+  b)
+    build
+    ;;
+  p)
+    push
+    ;;
+  *)
+    usage
+    ;;
+esac
+
+exit $?
